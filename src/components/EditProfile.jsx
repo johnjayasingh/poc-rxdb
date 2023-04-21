be1f8f5bf11c9e3d516246400da7fb5f15cd7122
John <johnjayasinghs@gmail.com>
2023-04-22 01:34:23 +0530

    layout rendered

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 
 export default function EditProfile(props) {
+
+  const items = [];
+
+  for (const sub of props.data.subscribe) {
+    items.push(
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>{sub.key}</Form.Label>
+        <Form.Control type="text" placeholder={sub.key} defaultValue={sub.value} />
+      </Form.Group>
+    )
+  }
+
   return (
     <div className="my-5 ">
       <h6>{props.name}</h6>
+      <Form>
+
+        {items}
+      </Form>
     </div>
   )
 }
